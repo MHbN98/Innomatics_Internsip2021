23e2ff476c0a5e82d53975142e30f0db951f4cc4
Mohammed Haaris <76543031+MHbN98@users.noreply.github.com>
2021-10-16 15:09:43 +0530

    Add files via upload

---
+++ Internsip_assinment/Division.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input("Enter n Value"))
+    for i in range(0,n) :
+        b = i*i
+        print(b)
+
---
+++ Internsip_assinment/aretimatic.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    a = int(input("Enter a Value"))
+    b = int(input("Enter b Value"))
+    print(a//b)
+    print(a/b)
+
